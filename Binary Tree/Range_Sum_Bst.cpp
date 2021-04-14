da5d67f652783ead6405a0a2c9712957fb09c534
Sravan Kumar <66896592+sravangorati2001@users.noreply.github.com>
2021-04-14 19:20:45 +0530

    Create Range_Sum_Bst.cpp

---
+++ Binary Tree/Range_Sum_Bst.cpp
@@ -0,0 +1,13 @@
+
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root==NULL) return 0;
+        if(root->val>high) return rangeSumBST(root->left, low, high);
+       else if(root->val<low) return rangeSumBST(root->right, low, high);
+    
+       else return root->val+rangeSumBST(root->left, low, high)+rangeSumBST(root->right, low, high);
+
+    }
+};
