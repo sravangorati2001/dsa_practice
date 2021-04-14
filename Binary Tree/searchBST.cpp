d0e240a3c467c70af1695f91a4cf44612c92bff7
Sravan Kumar <66896592+sravangorati2001@users.noreply.github.com>
2021-04-14 19:59:11 +0530

    Create searchBST.cpp

---
+++ Binary Tree/searchBST.cpp
@@ -0,0 +1,11 @@
+
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL) return NULL;
+       else if(root->val<val) return searchBST(root->right,val);
+       else if(root->val>val) return searchBST(root->left,val);
+        return root;
+    }
+};
