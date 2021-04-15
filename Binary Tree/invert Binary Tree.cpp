bd7c412149364c37c95058dd1b1ec0c34e9bf7e5
Sravan Kumar <66896592+sravangorati2001@users.noreply.github.com>
2021-04-15 11:40:01 +0530

    Update invert Binary Tree.cpp

--- Binary Tree/invert Binary Tree.cpp
+++ Binary Tree/invert Binary Tree.cpp
@@ -1,5 +1,6 @@
 
 // just reversing left and right childrens of every node
+//recursive
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -7,6 +8,26 @@ public:
         swap(root->left,root->right);
         invertTree(root->left);
         invertTree(root->right);
+        return root;
+    }
+};
+
+// iterative
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL) return NULL;
+       stack<TreeNode*> s;
+        s.push(root);
+        while(s.size()){
+            TreeNode *cur=s.top();
+            s.pop();
+            if(cur){
+                swap(cur->right,cur->left);
+                s.push(cur->left);
+                s.push(cur->right);
+            }
+        }
         return root;
     }
 };
