52c968170e7a0c3704bc27c439866129968a98de
Sravan Kumar <66896592+sravangorati2001@users.noreply.github.com>
2021-04-15 10:39:13 +0530

    Update Univalued Binary Tree.cpp

--- Binary Tree/Univalued Binary Tree.cpp
+++ Binary Tree/Univalued Binary Tree.cpp
@@ -19,3 +19,10 @@ public:
         return (root->val==root->left->val and root->val==root->right->val and isUnivalTree(root->left) and isUnivalTree(root->right));
     }
 };
+
+ bool isUnivalTree(TreeNode* root) {
+         if(root==NULL) return true;
+         if(root->right and root->val!=root->right->val) return false;
+         if(root->left and root->val!=root->left->val) return false;
+        return (isUnivalTree(root->left) and isUnivalTree(root->right));
+    }
